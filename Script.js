0b053ce08579c6a0f79eabf725611a4282eb11bd
jameschiagozie221-sketch <jameschiagozie221@gmail.com>
2026-04-24 21:12:24 +0100

    Add contact Alert function for button click alerts

---
+++ Script.js
@@ -0,0 +1,8 @@
+// Track which button dem click
+function contactAlert(type) {
+  if(type === 'work') {
+    alert('👑 Thank you! You go enter WhatsApp now. Tell me about your business website project. Prince Anointed ready to build! 💼');
+  } else {
+    alert('🦁 Thank you for suggestion! You go enter WhatsApp now. Tell me which animal + fun fact. I go add am to site! 🎉');
+  }
+}
